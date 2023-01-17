cde22719222a3a0b802c673d4d4dd057b5a49add
pgpetrov2001 <pgpetrov2001@gmail.com>
2023-01-17 13:51:34 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -18,7 +19,7 @@ char *strcatchr(char *str, char c) {
         return buf;
     }
     int len = strlen(str);
- 
+
     char *newstr = malloc(len+2);
     strcpy(newstr, str);
     free(str);
@@ -28,7 +29,7 @@ char *strcatchr(char *str, char c) {
 }
 
 bool is_special_char(char c) {
-    return isspace(c) || c == '&' || c == '\'' || c == ';';
+    return isspace(c) || c == '&' || c == '\'' || c == ';' || c == '>' || c == '<';
 }
 
 size_t getcnttokens(const char *line, size_t len) {
@@ -56,7 +57,7 @@ void syntax_error(const char *msg) {
 }
 
 char **gettokens(const char *line, size_t len, bool **is_control_out) {
-	is_control_out = NULL;
+    *is_control_out = NULL;
     int cnt = getcnttokens(line, len);
     //calloc fills values with 0s
     char **tokens = calloc(cnt + 1, sizeof(char*));
@@ -73,17 +74,12 @@ char **gettokens(const char *line, size_t len, bool **is_control_out) {
         } else {
             if (inSingleQuotes || escaping) {
                 tokens[currtoken] = strcatchr(tokens[currtoken], line[i]);
-                escaping = false;
+ 
+escaping = false;
             } else {
                 if (line[i] == '\\') {
                     escaping = true;
-                } else if (line[i] == ';') {
-                    if (tokens[currtoken]) ++currtoken;
-                    is_control[currtoken] = 1;
-                    tokens[currtoken] = malloc(2);
-                    strcpy(tokens[currtoken], ";");
-
-} else if (line[i] == '&') {
+                } else if (line[i] == '&') {
                     if (tokens[currtoken] && tokens[currtoken][0] == '&') {
                         if (strcmp(tokens[currtoken], "&&") == 0) {
                             syntax_error("unexpected character &");
@@ -100,6 +96,13 @@ char **gettokens(const char *line, size_t len, bool **is_control_out) {
                     }
                 } else if (isspace(line[i])) {
                     if (tokens[currtoken]) currtoken++;
+                } else if (is_special_char(line[i])) {
+                    if (tokens[currtoken]) ++currtoken;
+                    is_control[currtoken] = 1;
+                    tokens[currtoken] = malloc(2);
+                    char buf[2];
+                    buf[0] = line[i]; buf[1] = 0;
+                    strcpy(tokens[currtoken], buf);
                 } else {
                     if (is_control[currtoken]) currtoken++;
                     tokens[currtoken] = strcatchr(tokens[currtoken], line[i]);
@@ -114,94 +117,128 @@ char **gettokens(const char *line, size_t len, bool **is_control_out) {
 }
 
 void signal_error(const char *msg) {
-	write(2, msg, strlen(msg));
+    write(2, msg, strlen(msg));
 }
 
-void general_exec(char *cmd) {
-	if (execvp(cmd[0], cmd) == -1) {
-		write(2, "Command ", strlen("Command "));
-		write(2, cmd[0], strlen(cmd[0]));
-		write(2, " not found in path");
-	}
+void general_exec(const char **cmd, char *inredir, char *outredir) {
+    if (inredir) {
+        int fdr = open(inredir, O_RDONLY);
+        if (fdr == -1) {
+            write(2, "Could not open file", strlen("Could not open file"));
+            write(2, inredir, strlen(inredir));
+            write(2, " for writing\n", strlen(" for writing\n"));
+            return;
+        }
+        close(0); dup(fdr);
+    }
+    if (outredir) {
+        int fdw = open(outredir, O_WRONLY|O_CREAT|O_TRUNC, 0777);
+        if (fdr == -1) {
+            write(2, "Could not open file", strlen("Could not open file"));
+            write(2, inredir, strlen(inredir));
+            write(2, " for writing\n", strlen(" for writing\n"));
+
+return;
+        }
+        close(1); dup(fdw);
+    }
+    if (execvp(cmd[0], cmd) == -1) {
+        write(2, "Command ", strlen("Command "));
+        write(2, cmd[0], strlen(cmd[0]));
+        write(2, " not found in path");
+    }
 }
 
-int execute_singleton_command(const char *cmd) {
-	int pid;
-	if (pid = fork()) {
-		int status;
-		waitpid(pid, &status);
-		return status;
-	} else {
-		general_exec(cmd);
-	}
+int execute_singleton_command(const char **cmd, char *inredir, char *outredir) {
+    int pid;
+    if (pid = fork()) {
+        int status;
+        waitpid(pid, &status, 0);
+        fflush(stdout);
+        return status;
+    } else {
+        general_exec(cmd, *inredir, *outredir);
+    }
 }
 
-void execute_singleton_command_async(const char *cmd) {
-	int pid;
-	if (pid = fork()) {
-		char msg[20];
-		sprintf(msg, "[%d]\n", pid);
-		write(1, msg, strlen(msg));
-	} else {
-		general_exec(cmd);
-	}
+void execute_singleton_command_async(const char **cmd, char *inredir, char *outredir) {
+    int pid;
+    if (pid = fork()) {
+        char msg[20];
+        sprintf(msg, "[%d]\n", pid);
+        write(1, msg, strlen(msg));
+    } else {
+        general_exec(cmd, *inredir, *outredir);
+    }
 }
 
 void execute_command(char **tokens, bool *is_control) {
-	int num_control = 0;
-	int num_tokens = 0;
-	{
-		char **it;
-		bool async_syntax_error;
-		for (it=tokens, num_tokens=0; *it; ++it, ++num_tokens) 
-		{
-			async_syntax_error = 0;
-			if (is_control[num_tokens]) {
-				if (num_tokens > 0 && is_control[num_tokens-1]) {
-					syntax_error("syntax error with 2 consecutive control tokens");
-					return;
-				}
-				if (strcmp(*it, "&") == 0) async_syntax_error = 1;
-				++num_control;
-			}
-		}
-		if (async_syntax_error) {
-			syntax_error("async operator & is only at end of input");
-			return;
-		}
-	}
-	if (num_tokens == 0) return;
+    int num_control = 0;
+    int num_tokens = 0;
+    {
+        char **it;
+        for (it=tokens, num_tokens=0; *it; ++it, ++num_tokens)
+        {
+            if (is_control[num_tokens]) {
+                if (num_tokens > 0 && is_control[num_tokens-1]) {
+                    syntax_error("syntax error with 2 consecutive control tokens");
+                    return;
+                }
+                ++num_control;
+            }
+        }
+    }
+    if (num_tokens == 0) return;
     if (is_control[0]) {
         syntax_error("unexpected token at start");
         return;
     }
-	if (is_control[num_tokens-1] && (strcmp(tokens[num_tokens-1], "&") == 0 || strcmp(tokens[num_tokens-1], ";") == 0)) {
-		syntax_error("unexpected token at end of input");
-		return;
-	}
-    char **cmd         = calloc((num_tokens+1) * sizeof(void*));
-	char **cmd_end = cmd;
-	//char ***pipe_chain = calloc(num_control * sizeof(void*));
-	char **it             = tokens;
-	char **cmd_it         = cmd;
-	//char ***pipe_chain_it = pipe_chain;
+    if (is_control[num_tokens-1] && strcmp(tokens[num_tokens-1], "&&") == 0) {
+        syntax_error("unexpected token at end of input");
+        return;
+    }
+    char **cmd         = calloc(num_tokens+1, sizeof(void*));
+    char **cmd_end = cmd;
+    //char ***pipe_chain = calloc(num_control * sizeof(void*));
+
+char **it             = tokens;
+    char **cmd_it         = cmd;
+    //char ***pipe_chain_it = pipe_chain;
+    bool reading_out_redir = 0;
+    bool reading_in_redir = 0;
+    char *inredir = NULL;
+    char *outredir = NULL;
     for (; *it; ++it) {
         char *token = *it;
         if (is_control[it-tokens]) {
-			if (strcmp(token, "&&") == 0) {
-				if (execute_singleton_command(cmd)) return;
-			} else if (strcmp(token, ";") == 0) {
-				execute_singleton_command(cmd);
-			} else if (strcmp(token, "&") == 0) {
-				execute_singleton_command_async(cmd);
-			} else {
-				assert(false);
-			}
-			cmd = cmd_end;
+            if (strcmp(token, "&&") == 0) {
+                if (execute_singleton_command(cmd, inredir, outredir)) return;
+            } else if (strcmp(token, ";") == 0) {
+                execute_singleton_command(cmd, inredir, outredir);
+            } else if (strcmp(token, "&") == 0) {
+                execute_singleton_command_async(cmd);
+            } else if (strcmp(token, ">") == 0) {
+                reading_out_redir = 1;
+            } else if (strcmp(token, "<") == 0) {
+                reading_in_redir = 1;
+            } else {
+                assert(false);
+            }
+            cmd = cmd_end;
         } else {
-			*(cmd_end++) = token;
+            if (reading_in_redir) {
+                inredir = token;
+                reading_in_redir = 0;
+            } else if (reading_out_redir) {
+                outredir = token;
+                reading_out_redir = 0;
+            } else {
+                *(cmd_end++) = token;
+            }
         }
     }
+
+    if (cmd[0]) execute_singleton_command(cmd);
 }
 
 int main(int argc, char *argv[]) {
@@ -217,7 +254,12 @@ int main(int argc, char *argv[]) {
             free(line);
             continue;
         }
-        execute_command(tokens);
+        char **it = tokens;
+        for (; *it; ++it) {
+            printf("token %s\n", *it);
+        }
+        fflush(stdout);
+        execute_command(tokens, is_control);
     }
     return 0;
 }
